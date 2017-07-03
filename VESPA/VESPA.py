0b7b2938ecc54899d921dd03cbc69e850480b430
mminin <mminin2010@gmail.com>
2017-07-03 21:25:33 +0200

    update VESPA plugin to not use TK, only Qt4

--- VESPA/VESPA.py
+++ VESPA/VESPA.py
@@ -20,40 +20,19 @@
  *                                                                         *
  ***************************************************************************/
 """
-from qgis.core    import QgsProject
+from qgis.core    import QgsProject, QgsMessageLog
 from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
 from PyQt4.QtGui  import QAction,   QIcon,       QMenu,    QMessageBox
+from PyQt4 import QtCore
+from PyQt4 import QtGui
 import resources
 import threading
-
-import Tkinter as tk
-import tkMessageBox
 
 from astropy.vo.samp import SAMPHubServer
 from astropy.vo.samp.hub import WebProfileDialog
 
 from .scriptReceiver import scriptReceiver
-
-class TkWebProfileDialog(WebProfileDialog):
-    def __init__(self, root):
-        self.SampMESSAGE = "A Web application which declares to be\n\nName: {name}\n"+\
-            "Origin: {origin}\n\nis requesting to be registered with the SAMP Hub.  "+\
-            "Pay attention\nthat if you permit its registration, "+\
-            "such application will acquire\nall current user privileges, like file "+\
-            "read/write.\n\nDo you give your consent?"
-        self.root = root
-        self.root.wm_title("SAMP HUB")
-        self.wait_for_dialog()
-
-    def wait_for_dialog(self):
-        self.handle_queue()
-        self.root.after(100, self.wait_for_dialog)
-
-    def show_dialog(self, samp_name, details, client, origin):
-        SampMESSAGE="abc"
-        text = self.SampMESSAGE.format(name=samp_name, origin=origin)
-        if tkMessageBox.askyesno('SAMP Hub', text, default=tkMessageBox.NO): self.consent()
-        else: self.reject()
+from .myHUB import *
 
 class VESPA:
     def __init__(self, iface):
@@ -92,26 +71,9 @@ class VESPA:
             callback  = self.startReceiver)
 
     def startSampHub(self):
-        def runSampHub():
-            self.root=tk.Tk()
-            tk.Label(self.root, text="HUB root window. \n Close this terminates \n SAMP Hub Server",
-                     font=("Helvetica", 12), justify=tk.CENTER).pack(pady=5)
-            self.root.geometry("200x110")
-            self.root.update()
-            self.h = SAMPHubServer(web_profile_dialog=TkWebProfileDialog(self.root))
-            self.h.start()
-            self.root.mainloop()
-            try:self.root.mainloop() # Main GUI loop 
-            except KeyboardInterrupt:pass ## DO I really NEED TRY HERE? How does one even interrupt it? it's a window...
-            self.h.stop()
-        t=threading.Thread(target=runSampHub)
-        t.daemon=True
-        t.start()
+        self.sh=HUBrunner(self.iface, self.pinstance)
+        self.sh.run()
 
     def startReceiver(self):
-        sr=scriptReceiver(self.iface, self.pinstance).run
-##        t=threading.Thread(target=sr)
-##        t.daemon=True
-##        t.start()
         self.r=scriptReceiver(self.iface, self.pinstance)
         self.r.run()
